b9ff2a853fd9dbc9d4cbc6f0dc1d214560cf47e5
linyiming <2391660155@qq.com>
2024-03-20 21:19:41 +0800

    图标初始化

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,37 @@
+import * as echarts from 'echarts';
+import {useEffect, useRef} from "react";
+
+
 const Home = () => {
-  return <div>this is home</div>
+  const chartRef = useRef(null)
+
+  useEffect(() => {
+    //保证dom可用
+    const chartDom = chartRef.current;
+    const myChart = echarts.init(chartDom);
+
+    const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    };
+
+    option && myChart.setOption(option);
+  }, []);
+
+  return (<div>
+    <div ref={chartRef} style={{width: '500px', height: '400px'}}></div>
+  </div>)
 }
 
 export default Home
