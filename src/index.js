d5f2b2e6bf5a9d2b99c144f306d3bcf5748efdc0
linyiming <2391660155@qq.com>
2024-03-21 20:47:18 +0800

    引入富文本编辑器

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router}/>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
 );
 
